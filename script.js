a2802a04117141d9e6937f3ebf1467be04d43b73
trastanechora <trastanechora@gmail.com>
2019-02-27 21:57:48 +0700

    changed to other version

--- script.js
+++ script.js
@@ -1,62 +1,111 @@
-var playArea = document.createElement('div'),
-        ship = document.createElement('div'),
-        shipPos = {
-            x: 0,
-            y: 0
-        },
-        shipSpeed = 10,
-        key = {
-            right: false,
-            left: false,
-            up: false,
-            down: false
-        },
-        shipWidth = ship.offsetWidth,
-        shipHeight = ship.offsetHeight;
 
-document.getElementById("main").appendChild(playArea);
+var myGamePiece;
+var myObstacle;
 
-playArea.classList.add('playArea');
+function startGame() {
+    myGamePiece = new component(10, 20, "red", 10, 120);
+    myObstacle  = new component(10, 200, "green", 300, 120);    
+    myGameArea.start();
+}
 
-
-document.getElementById("main").appendChild(ship);
-ship.classList.add('ship');
-shipPos.x = (playArea.offsetWidth / 2 + playArea.offsetLeft) - (ship.offsetWidth / 2);
-shipPos.y = (playArea.offsetHeight + playArea.offsetTop) - (ship.offsetHeight * 2);
-playArea.leftBoundary = playArea.offsetLeft + 10;
-playArea.rightBoundary = (playArea.offsetLeft + playArea.offsetWidth - 10) - ship.offsetWidth;
-playArea.topBoundary = playArea.offsetTop + 10;
-playArea.bottomBoundary = (playArea.offsetTop + playArea.offsetHeight - 10) - ship.offsetHeight;
-
-// function keyDown(e) {
-//     if (e.keyCode === 39) {
-//         key.right = true;
-//     } else if (e.keyCode === 37) {
-//         key.left = true;
-//     }
-//     if (e.keyCode === 38) {
-//         key.up = true;
-//     } else if (e.keyCode === 40) {
-//         key.down = true;
-//     }
-// }
-// if (shipPos.y <= 470) {
-//     key.up = true;
-// }
-
-function keyDown(e) {
-    if (e.keyCode === 39) {
-        key.right = true;
-    } else if (e.keyCode === 37) {
-        key.left = true;
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 1300;
+        this.canvas.height = 700 ;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+        loop();
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
     }
-    if (e.keyCode === 38) {
-        key.up = true;
-    } else if (shipPos.y <= 470) {
-        if (e.keyCode === 40) {
-            key.down = true;
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.crashWith = function(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crash = false;
         }
+        return crash;
     }
+}
+
+function updateGameArea() {
+    if (myGamePiece.crashWith(myObstacle)) {
+        myGameArea.stop();
+    } else {
+        myGameArea.clear();
+        myObstacle.update();
+        myGamePiece.x += myGamePiece.speedX;
+        myGamePiece.y += myGamePiece.speedY;    
+        myGamePiece.update();
+    }
+}
+
+function moveup() {
+    myGamePiece.speedY = -1; 
+}
+
+function movedown() {
+    myGamePiece.speedY = 1; 
+}
+
+function moveleft() {
+    myGamePiece.speedX = -1; 
+}
+
+function moveright() {
+    myGamePiece.speedX = 1; 
+}
+
+function clearmove() {
+    myGamePiece.speedX = 0; 
+    myGamePiece.speedY = 0; 
+}
+
+function gravitation() {
+    myGamePiece.speedY = 1; 
+}
+
+document.addEventListener('keydown', keyDown, false);
+document.addEventListener('keyup', keyUp, false);
+
+function loop() {
+    gravitation();
+    move()
+    setTimeout(loop, 1000 / 60);
+}
+
+var key = {
+    right: false,
+    left: false,
+    up: false,
+    down: false
 }
 
 function keyUp(e) {
@@ -72,45 +121,29 @@ function keyUp(e) {
     }
 }
 
-function moveShip() {
+function keyDown(e) {
+    if (e.keyCode === 39) {
+        key.right = true;
+    } else if (e.keyCode === 37) {
+        key.left = true;
+    }
+    if (e.keyCode === 38) {
+        key.up = true;
+    } else if (e.keyCode === 40) {
+        key.down = true;
+    }
+}
+
+var speed = 2
+function move() {
     if (key.right === true) {
-        shipPos.x += shipSpeed;
+        myGamePiece.x += speed;
     } else if (key.left === true) {
-        shipPos.x -= shipSpeed;
+        myGamePiece.x -= speed;
     }
     if (key.up === true) {
-        shipPos.y -= shipSpeed;
+        myGamePiece.y -= speed;
     } else if (key.down === true) {
-        shipPos.y += shipSpeed;
+        myGamePiece.y += speed;
     }
-    if (shipPos.x < playArea.leftBoundary) {
-        shipPos.x = playArea.leftBoundary;
-    }
-    if (shipPos.x > playArea.rightBoundary) {
-        shipPos.x = playArea.rightBoundary;
-    }
-    if (shipPos.y < playArea.topBoundary) {
-        shipPos.y = playArea.topBoundary;
-    }
-    if (shipPos.y > playArea.bottomBoundary) {
-        shipPos.y = playArea.bottomBoundary;
-    }
-    ship.style.left = shipPos.x + 'px';
-    ship.style.top = shipPos.y + 'px';
 }
-
-gravitation = () => {
-    shipPos.y += 5
-}
-
-document.addEventListener('keydown', keyDown, false);
-document.addEventListener('keyup', keyUp, false);
-
-function loop() {
-    moveShip();
-    console.log(shipPos.y)
-    gravitation();
-    setTimeout(loop, 1000 / 60);
-}
-
-loop();
